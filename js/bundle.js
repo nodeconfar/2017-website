5fd1db5b32cd059f375cf5ec0f7e9dd022e04e84
iamcgd <hola@iamcgd.com>
2017-07-25 16:13:15 -0300

    Jeff Lembeck
    + new speaker
    + smoothscroll on >
    + uncss

--- js/bundle.js
+++ js/bundle.js
@@ -42,6 +42,41 @@ $(window).scroll(function (event) {
 });
 $(window).scroll();
 
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 700, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 // smooth scroll
 // Cache selectors
 // Cache selectors
@@ -117,7 +152,7 @@ setInterval("showDate()", 1000);
 //menu
 $("#toggle-menu").click(function(){
     $(this).toggleClass("menu-opened");
-    $("#menu").toggleClass("fadeIn");
+    $("#menu").toggleClass("animated fadeIn");
     $("#menu").toggleClass("dn");
 });
 $("#menu li a").click(function(){
